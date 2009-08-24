59e3f8a5ae9f710762787a461656699f728be7b6
Jeff Burnz <git@adaptivethemes.com>
2009-08-24 00:16:36 +0000

    add $head to head, was accidentally removed at some stage

--- templates/page/maintenance-page.tpl.php
+++ templates/page/maintenance-page.tpl.php
@@ -14,6 +14,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 <head>
   <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>      
