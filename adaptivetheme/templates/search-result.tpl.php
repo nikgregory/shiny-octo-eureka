86c7cf0d88111aa12a9da56542f7b0181f8c979e
Jeff Burnz <git@adaptivethemes.com>
2009-12-07 03:11:27 +0000

    ops, fix info_split display error

--- adaptivetheme/templates/search-result.tpl.php
+++ adaptivetheme/templates/search-result.tpl.php
@@ -56,25 +56,30 @@
 
     <?php if ($info_split) : ?>
       <p class="search-info">
-  
+        <?php $separator = ''; ?>
+        
         <?php if (isset($info_split['type'])) : ?>
           <span class="search-info-type"><?php print $info_split['type']; ?></span>
+          <?php $separator = $info_separator; ?>
         <?php endif; ?>
 
         <?php if (isset($info_split['user'])) : ?>
-          <span class="search-info-user"><?php print $info_separator . $info_split['user']; ?></span>
+          <span class="search-info-user"><?php print $separator . $info_split['user']; ?></span>
+          <?php $separator = $info_separator; ?>
         <?php endif; ?>
    
         <?php if (isset($info_split['date'])) : ?>
-          <span class="search-info-date"><?php print $info_separator . $info_split['date']; ?></span>
+          <span class="search-info-date"><?php print $separator . $info_split['date']; ?></span>
+          <?php $separator = $info_separator; ?>
         <?php endif; ?>
 
         <?php if (isset($info_split['comment'])) : ?>
-          <span class="search-info-comment"><?php print $info_separator . $info_split['comment']; ?></span>
+          <span class="search-info-comment"><?php print $separator . $info_split['comment']; ?></span>
+          <?php $separator = $info_separator; ?>
         <?php endif; ?>
 
         <?php if (isset($info_split['upload'])) : ?>
-          <span class="search-info-upload"><?php print $info_separator . $info_split['upload']; ?></span>
+          <span class="search-info-upload"><?php print $separator . $info_split['upload']; ?></span>
         <?php endif; ?>
       </p>
     <?php endif; ?>
