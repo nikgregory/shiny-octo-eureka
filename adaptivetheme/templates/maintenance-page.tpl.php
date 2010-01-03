413af179a1a880815946460102f826b14bdd1800
Jeff Burnz <git@adaptivethemes.com>
2010-01-03 01:37:53 +0000

    fix maintenance-page.tpl to be like the normal page tpl, add comments to explain how it works in AT

--- adaptivetheme/templates/maintenance-page.tpl.php
+++ adaptivetheme/templates/maintenance-page.tpl.php
@@ -1,10 +1,18 @@
 <?php // $Id$
 // adaptivethemes.com
-
 /**
 * @file maintenance-page.tpl.php
 *
 * Theme implementation to display a single Drupal page while off-line.
+*
+* Adaptivetheme maintenance page does not include sidebars by default, nor
+* does it print messages, tabs or anything else that typically you would
+* not see on a maintenance page. If you require any of these additional variables
+* you will need to add them. Also the columns layout has been totally removed.
+*
+* themename_preprocess is disabled when the database is not active (see
+* template.php). This is because it calls many functions that rely on the database
+* being active and will cause errors when the maintenance page is viewed.
 *
 * @see template_preprocess()
 * @see template_preprocess_maintenance_page()
@@ -20,33 +28,37 @@
 </head>
 <body class="<?php print $body_classes; ?>">
   <div id="container">
-    <div id="header" class="clear-block">
+    <div id="header" class="clearfix">
       <?php if ($logo or $site_name or $site_slogan): ?>
         <div id="branding">
-          <?php if (!empty($logo)): ?>
-            <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-            </a>
-          <?php endif; ?>
-          <?php if (!empty($site_name)): ?>
-            <div id="site-name"><strong>
-              <a href="<?php print $base_path ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-            </strong></div>
-          <?php endif; ?>
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php if ($logo or $site_name): ?>
+            <div class="logo-site-name"><strong>
+              <?php if (!empty($logo)): ?>
+                <span id="logo">
+                  <a href="<?php print $base_path; ?>" title="<?php print t('Home page'); ?>" rel="home">
+                    <img src="<?php print $logo; ?>" alt="<?php print t('Home page'); ?>" />
+                  </a>
+                </span>
+              <?php endif; ?>
+              <?php if (!empty($site_name)): ?>
+                <span id="site-name">
+                  <a href="<?php print $base_path ?>" title="<?php print t('Home page'); ?>" rel="home">
+                    <?php print $site_name; ?>
+                  </a>
+                </span>
+              <?php endif; ?>
+            </strong></div> <!-- /logo and site name -->
+            <?php if ($site_slogan): ?>
+              <div id="site-slogan"><?php print $site_slogan; ?></div>
+            <?php endif; ?> <!-- /slogan -->
           <?php endif; ?>
         </div> <!-- /branding -->
       <?php endif; ?>
     </div> <!-- /header -->
-    <div id="columns"><div class="columns-inner clear-block">
-      <div id="content-column"><div class="content-inner">
-        <div id="main-content">
-          <?php if ($title): ?><h1 id="page-title"><?php print $title; ?></h1><?php endif; ?>
-          <div id="content"><?php print $content; ?></div>
-        </div> <!-- /main-content -->
-      </div></div> <!-- /content-column -->
-    </div></div> <!-- /columns -->
+    <div id="main-content">
+      <?php if ($title): ?><h1 id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <div id="content"><?php print $content; ?></div>
+    </div> <!-- /main-content -->
   </div> <!-- /container -->
   <?php print $closure ?>
 </body>
