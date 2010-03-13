0772baf3bc3a6132b20a3a2cc993cb89d2526255
Jeff Burnz <git@adaptivethemes.com>
2010-03-13 09:06:13 +0000

    *** empty log message ***

--- adaptivetheme/templates/views-view-unformatted.tpl.php
+++ adaptivetheme/templates/views-view-unformatted.tpl.php
@@ -8,11 +8,18 @@
  * @ingroup views_templates
  */
 ?>
+<?php
+// Conditionally add CSS classes.
+$classes = array();
+if (theme_get_setting(cleanup_views_unformatted_classes)) {
+  $classes = ' ' . $classes[$id];
+}
+?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div class="views-row-item <?php print $classes[$id]; ?>">
+  <div class="views-row-item<?php print $classes ? classes : ''; ?>">
     <?php print $row; ?>
   </div>
 <?php endforeach; ?> <!-- /views-view-unformatted -->
