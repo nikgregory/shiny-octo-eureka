93e2d01260416fe3b6c00aae0f368f812725ae34
Jeff Burnz <git@adaptivethemes.com>
2010-06-13 13:57:44 +0000

    rework blocks to remove block-inner div

--- adaptivetheme/templates/block.tpl.php
+++ adaptivetheme/templates/block.tpl.php
@@ -1,5 +1,5 @@
-<?php // $Id$
-// adaptivethemes.com
+<?php
+// $Id$
 
 /**
  * @file
@@ -43,19 +43,14 @@
  */
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="block-inner">
-
-    <?php print render($title_prefix); ?>
-    <?php if ($block->subject): ?>
-      <h2<?php print $title_attributes; ?>>
-        <?php print $block->subject; ?>
-      </h2>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-
-    <div<?php print $content_attributes; ?>>
-      <?php print $content ?>
-    </div>
-
+  <?php print render($title_prefix); ?>
+  <?php if ($block->subject): ?>
+    <h2<?php print $title_attributes; ?>>
+      <?php print $block->subject; ?>
+    </h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <div<?php print $content_attributes; ?>>
+    <?php print $content ?>
   </div>
 </div> <!-- /block -->
