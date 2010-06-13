6aef8d7aa538974ee77aa7621ba718dc1c63b4a9
Jeff Burnz <git@adaptivethemes.com>
2010-06-13 01:08:05 +0000

    update gpanels to use print render

--- adaptivetheme/gpanels/5col-5x20.php
+++ adaptivetheme/gpanels/5col-5x20.php
@@ -1,54 +1,49 @@
-<?php // $Id$
-// adaptivethemes.com
+<?php
+// $Id$
 
 /**
  * @file 5col-5x20.php
  * Gpanel code snippet to display 5x20% width regions as columns.
  *
- * GPanels are drop in multi-column snippets for displaying blocks in 
+ * GPanels are drop in multi-column snippets for displaying blocks in
  * vertical columns, such as 2 columns, 3 columns or 4 columns.
  *
  * How to use a Gpanel:
  * 1. Copy and paste a Gpanel into your page.tpl.php file.
  * 2. Uncomment the matching regions in your subthemes info file.
- * 3. Clear the cache (in Performance settings) to refresh the theme registry. 
+ * 3. Clear the cache (in Performance settings) to refresh the theme registry.
  *
- * Now you can add blocks to the regions as per normal. The layout CSS for 
+ * Now you can add blocks to the regions as per normal. The layout CSS for
  * these regions is already set up so there is nothing more to do.
- * 
+ *
  * Gpanels are fluid, meaning they stretch and compress with the page width.
  *
  * Region Variables:
- * 
- * regions[five_col_first]   = 5col Gpanel col 1
- * regions[five_col_second]  = 5col Gpanel col 2
- * regions[five_col_third]   = 5col Gpanel col 3
- * regions[five_col_fourth]  = 5col Gpanel col 4
- * regions[five_col_fifth]   = 5col Gpanel col 5
+ * $page['five_col_first']  outputs the "5col Gpanel col 1" region.
+ * $page['five_col_second'] outputs the "5col Gpanel col 2" region.
+ * $page['five_col_third']  outputs the "5col Gpanel col 3" region.
+ * $page['five_col_fourth'] outputs the "5col Gpanel col 4" region.
+ * $page['five_col_fifth']  outputs the "5col Gpanel col 5" region.
  */
 ?>
-
-<!--//   Five column Gpanel   //-->
-<?php if ($five_col_first or $five_col_second or $five_col_third or $five_col_fourth or $five_col_fifth): ?>
+<!-- Five column Gpanel -->
+<?php if ($page['five_col_first'] || $page['five_col_second'] || $page['five_col_third'] || $page['five_col_fourth'] || $page['five_col_fifth']): ?>
   <div class="five-col-5x20 gpanel clearfix">
     <div class="section region col-1 first"><div class="inner">
-      <?php if ($five_col_first): print $five_col_first; endif; ?>
+      <?php if ($page['five_col_first']): print render($page['five_col_first']); endif; ?>
     </div></div>
     <div class="section region col-2"><div class="inner">
-      <?php if ($five_col_second): print $five_col_second; endif; ?>
+      <?php if ($page['five_col_second']): print render($page['five_col_second']); endif; ?>
     </div></div>
     <div class="section region col-3"><div class="inner">
-      <?php if ($five_col_third): print $five_col_third; endif; ?>
+      <?php if ($page['five_col_third']): print render($page['five_col_third']); endif; ?>
     </div></div>
     <div class="section region col-4"><div class="inner">
-      <?php if ($five_col_fourth): print $five_col_fourth; endif; ?>
+      <?php if ($page['five_col_fourth']): print render($page['five_col_fourth']); endif; ?>
     </div></div>
     <div class="section region col-5 last"><div class="inner">
-      <?php if ($five_col_fifth): print $five_col_fifth; endif; ?>
-    </div></div> 
+      <?php if ($page['five_col_fifth']): print render($page['five_col_fifth']); endif; ?>
+    </div></div>
   </div>
 <?php endif; ?>
 <!--/end Gpanel-->
-
-
-
--- adaptivetheme/gpanels/6col-6x16.php
+++ adaptivetheme/gpanels/6col-6x16.php
@@ -1,59 +1,53 @@
-<?php // $Id$
-// adaptivethemes.com
+<?php
+// $Id$
 
 /**
  * @file 6col-6x16.php
  * Gpanel code snippet to display 6x16% width regions as columns.
  *
- * GPanels are drop in multi-column snippets for displaying blocks in 
+ * GPanels are drop in multi-column snippets for displaying blocks in
  * vertical columns, such as 2 columns, 3 columns or 4 columns.
  *
  * How to use a Gpanel:
  * 1. Copy and paste a Gpanel into your page.tpl.php file.
  * 2. Uncomment the matching regions in your subthemes info file.
- * 3. Clear the cache (in Performance settings) to refresh the theme registry. 
+ * 3. Clear the cache (in Performance settings) to refresh the theme registry.
  *
- * Now you can add blocks to the regions as per normal. The layout CSS for 
+ * Now you can add blocks to the regions as per normal. The layout CSS for
  * these regions is already set up so there is nothing more to do.
- * 
+ *
  * Gpanels are fluid, meaning they stretch and compress with the page width.
  *
  * Region Variables:
- * 
- * regions[six_col_first]    = 6col Gpanel col 1
- * regions[six_col_second]   = 6col Gpanel col 2
- * regions[six_col_third]    = 6col Gpanel col 3
- * regions[six_col_fourth]   = 6col Gpanel col 4
- * regions[six_col_fifth]    = 6col Gpanel col 5
- * regions[six_col_sixth]    = 6col Gpanel col 6
+ * $page['six_col_first']  outputs the "6col Gpanel col 1" region.
+ * $page['six_col_second'] outputs the "6col Gpanel col 2" region.
+ * $page['six_col_third']  outputs the "6col Gpanel col 3" region.
+ * $page['six_col_fourth'] outputs the "6col Gpanel col 4" region.
+ * $page['six_col_fifth']  outputs the "6col Gpanel col 5" region.
+ * $page['six_col_sixth']  outputs the "6col Gpanel col 6" region.
  */
 ?>
-
-<!--//   Six column Gpanel   //-->
-<?php if ($six_col_first or $six_col_second or $six_col_third or $six_col_fourth or $six_col_fifth or $six_col_sixth): ?>
+<!-- Six column Gpanel -->
+<?php if ($page['six_col_first'] || $page['six_col_second'] || $page['six_col_third'] || $page['six_col_fourth'] || $page['six_col_fifth'] || $page['six_col_sixth']): ?>
   <div class="six-col-6x16 gpanel clearfix">
     <div class="section region col-1 first"><div class="inner">
-      <?php if ($six_col_first): print $six_col_first; endif; ?>
+      <?php if ($page['six_col_first']): print render($page['six_col_first']); endif; ?>
     </div></div>
     <div class="section region col-2"><div class="inner">
-      <?php if ($six_col_second): print $six_col_second; endif; ?>
+      <?php if ($page['six_col_second']): print render($page['six_col_second']); endif; ?>
     </div></div>
     <div class="section region col-3"><div class="inner">
-      <?php if ($six_col_third): print $six_col_third; endif; ?>
+      <?php if ($page['six_col_third']): print render($page['six_col_third']); endif; ?>
     </div></div>
     <div class="section region col-4"><div class="inner">
-      <?php if ($six_col_fourth): print $six_col_fourth; endif; ?>
+      <?php if ($page['six_col_fourth']): print render($page['six_col_fourth']); endif; ?>
     </div></div>
     <div class="section region col-5 last"><div class="inner">
-      <?php if ($six_col_fifth): print $six_col_fifth; endif; ?>
+      <?php if ($page['six_col_fifth']): print render($page['six_col_fifth']); endif; ?>
     </div></div>
     <div class="section region col-6 last"><div class="inner">
-      <?php if ($six_col_sixth): print $six_col_sixth; endif; ?>
+      <?php if ($page['six_col_sixth']): print render($page['six_col_sixth']); endif; ?>
     </div></div>
   </div>
 <?php endif; ?>
 <!--/end Gpanel-->
-
-
-
-
