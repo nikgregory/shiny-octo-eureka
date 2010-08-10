e584ec3a1649bfe461404baa6a41e8270ce693fe
Jeff Burnz <git@adaptivethemes.com>
2010-08-10 18:44:27 +0000

    fix repeating inline field labels [#878544]

--- adaptivetheme/templates/content-field.tpl.php
+++ adaptivetheme/templates/content-field.tpl.php
@@ -29,19 +29,22 @@
 <div class="field <?php print $field_name_css ?>">
   <?php if ($label_display == 'above') : ?>
     <h3 class="label"><?php print t($label) ?></h3>
-  <?php endif;?>
-    <?php $count = 1;
+  <?php elseif ($label_display =='inline'): ?>
+    <h3 class="label inline"><?php print t($label); ?>:&nbsp;</h3>
+  <?php endif; ?>
+  <?php
+    $count = 1;
     foreach ($items as $delta => $item) :
       if (!$item['empty']) : ?>
         <?php if (theme_get_setting(cleanup_fields_zebra)) { $zebra = $count % 2 ? 'odd' : 'even'; } ?>
         <?php if ($label_display == 'inline'): ?>
-          <h3 class="label inline"><?php print t($label) ?>:&nbsp;</h3>
           <div class="item inline<?php print $zebra ? ' ' . $zebra : '';?>"><?php print $item['view'] ?></div>
         <?php else: ?>
           <div class="item<?php print $zebra ? ' ' . $zebra : '';?>"><?php print $item['view'] ?></div>
         <?php endif;?>
       <?php $count++;
       endif;
-    endforeach;?>
+    endforeach;
+  ?>
 </div>
 <?php endif; ?> <!-- /content-field -->
