fcc142acd338c1b2946cfdb905110e3c402ceb81
Jeff Burnz <git@adaptivethemes.com>
2010-10-14 15:58:53 +0000

    update and fix the style schemes feature

--- adaptivetheme_subtheme/template.php
+++ adaptivetheme_subtheme/template.php
@@ -2,8 +2,32 @@
 // $Id$
 
 /**
- * @file template.php
+ * Add the Style Schemes if enabled.
+ * NOTE: You must make changes in your subthemes theme-settings.php file
+ * also to enable Style Schemes.
  */
+
+/* -- Delete this line if you want to enable Style Schemes
+
+// DONT TOUCH THIS FUNCTION!
+function get_at_styles() {
+  $scheme = theme_get_setting('style_schemes');
+  if (!$scheme) {
+    $scheme = 'style-default.css';
+  }
+  if (isset($_COOKIE["atstyles"])) {
+    $scheme = $_COOKIE["atstyles"];
+  }
+  return $scheme;
+}
+
+// ONLY MODIFY THIS - change 'adaptivetheme_subtheme' to your theme name:
+if (theme_get_setting('style_enable_schemes') == 'on') {
+  drupal_add_css(drupal_get_path('theme', 'adaptivetheme_subtheme') .'/css/schemes/'. get_at_styles(), 'theme');
+}
+
+// */
+
 
 /**
  * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
@@ -16,61 +40,61 @@
 /**
  * Override or insert variables into all templates.
  */
-/*
+/* -- Delete this line if you want to use these functions
 function adaptivetheme_subtheme_preprocess(&$vars, $hook) {
 }
 function adaptivetheme_subtheme_process(&$vars, $hook) {
 }
-*/
+// */
 
 /**
  * Override or insert variables into the html templates.
  */
-/*
+/* -- Delete this line if you want to use these functions
 function adaptivetheme_subtheme_preprocess_html(&$vars) {
-  // Uncomment the folowing line to add a conditional stylesheet for IE
-  // drupal_add_css(path_to_theme() . '/css/ie/lte-ie.css', array('weight' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
+  // Uncomment the folowing line to add a conditional stylesheet for IE 7 or less.
+  // drupal_add_css(path_to_theme() . '/css/ie/ie-lte-7.css', array('weight' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
 }
 function adaptivetheme_subtheme_process_html(&$vars) {
 }
-*/
+// */
 
 /**
  * Override or insert variables into the page templates.
  */
-/*
+/* -- Delete this line if you want to use these functions
 function adaptivetheme_subtheme_preprocess_page(&$vars) {
 }
 function adaptivetheme_subtheme_process_page(&$vars) {
 }
-*/
+// */
 
 /**
  * Override or insert variables into the node templates.
  */
-/*
+/* -- Delete this line if you want to use these functions
 function adaptivetheme_subtheme_preprocess_node(&$vars) {
 }
 function adaptivetheme_subtheme_process_node(&$vars) {
 }
-*/
+// */
 
 /**
  * Override or insert variables into the comment templates.
  */
-/*
+/* -- Delete this line if you want to use these functions
 function adaptivetheme_subtheme_preprocess_comment(&$vars) {
 }
 function adaptivetheme_subtheme_process_comment(&$vars) {
 }
-*/
+// */
 
 /**
  * Override or insert variables into the block templates.
  */
-/*
+/* -- Delete this line if you want to use these functions
 function adaptivetheme_subtheme_preprocess_block(&$vars) {
 }
 function adaptivetheme_subtheme_process_block(&$vars) {
 }
-*/
+// */
