5a126974fa1609997ac5fd8c73145b57cc22c8f5
Jeff Burnz <git@adaptivethemes.com>
2010-10-15 02:19:12 +0000

    cleanup css for admin outside the overlay

--- adaptivetheme_admin/templates/page.tpl.php
+++ adaptivetheme_admin/templates/page.tpl.php
@@ -19,7 +19,7 @@
       <?php if ($title): ?><h1 id="page-title"><?php print $title; ?></h1><?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php if ($primary_local_tasks): ?>
-        <nav id="primary-tasks"><ul class="tabs primary"><?php print render($primary_local_tasks); ?></ul><nav>
+        <div id="primary-tasks"><ul class="tabs primary"><?php print render($primary_local_tasks); ?></ul></div>
       <?php endif; ?>
     <?php endif; // end hide in overlay ?>
   </div>
@@ -29,7 +29,7 @@
   <div id="columns"><div class="columns-inner clearfix">
     <div id="content-column"><div class="content-inner">
       <?php if ($secondary_local_tasks): ?>
-        <nav id="secondary-tasks"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></nav>
+        <div id="secondary-tasks"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></div>
       <?php endif; ?>
       <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
       <?php print render($page['highlight']); ?>
