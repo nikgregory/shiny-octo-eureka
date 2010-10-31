6e93c8a85aea7388fc000fc4d9a2d72e79eb5d3d
Jeff Burnz <git@adaptivethemes.com>
2010-10-31 01:01:59 +0000

    always print the site name to preserve document outlines, just hide it if disabled

--- adaptivetheme/templates/page.tpl.php
+++ adaptivetheme/templates/page.tpl.php
@@ -11,10 +11,10 @@
       <div id="logo"><?php print $linked_site_logo; ?></div>
     <?php endif; ?>
 
-    <?php if ($linked_site_name || $site_slogan): ?>
-      <hgroup>
-        <?php if ($linked_site_name): ?>
-          <h1 id="site-name"><?php print $linked_site_name; ?></h1>
+    <?php if ($site_name || $site_slogan): ?>
+      <hgroup<?php if (!$site_slogan && $hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>>
+        <?php if ($site_name): ?>
+          <h1 id="site-name"<?php if ($hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>><?php print $site_name; ?></h1>
         <?php endif; ?>
         <?php if ($site_slogan): ?>
           <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
