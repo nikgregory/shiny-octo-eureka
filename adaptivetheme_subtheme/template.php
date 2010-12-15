fc9ba7a70cf986ea4c2fa8696d5addc7db304777
Jeff Burnz <git@adaptivethemes.com>
2010-12-15 11:56:01 +0000

    cleanup template.php and fix drupal_add_css for style schemes

--- adaptivetheme_subtheme/template.php
+++ adaptivetheme_subtheme/template.php
@@ -3,13 +3,11 @@
 
 /**
  * Add the Style Schemes if enabled.
- * NOTE: You must make changes in your subthemes theme-settings.php file
+ * NOTE: You MUST make changes in your subthemes theme-settings.php file
  * also to enable Style Schemes.
  */
-
-/* -- Delete this line if you want to enable Style Schemes
-
-// DONT TOUCH THIS FUNCTION!
+/* -- Delete this line if you want to enable style schemes.
+// DONT TOUCH THIS STUFF...
 function get_at_styles() {
   $scheme = theme_get_setting('style_schemes');
   if (!$scheme) {
@@ -20,20 +18,18 @@ function get_at_styles() {
   }
   return $scheme;
 }
-
-// ONLY MODIFY THIS - change 'pixture_reloaded' to your theme name:
 if (theme_get_setting('style_enable_schemes') == 'on') {
-  drupal_add_css(drupal_get_path('theme', 'pixture_reloaded') .'/css/schemes/'. get_at_styles(), 'theme');
+  $style = get_at_styles();
+  drupal_add_css(path_to_theme() . '/css/schemes/' . $style, array('group' => CSS_THEME));
 }
-
 // */
-
 
 /**
  * Preprocess and Process Functions SEE: http://drupal.org/node/254940#variables-processor
  * 1. Rename each function to match your subthemes name,
  *    e.g. if you name your theme "themeName" then the function
- *    name will be "themeName_preprocess_hook".
+ *    name will be "themeName_preprocess_hook". Tip - you can
+ *    search/replace on "adaptivetheme_subtheme".
  * 2. Uncomment the required function to use.
  */
 
@@ -41,23 +37,21 @@ if (theme_get_setting('style_enable_schemes') == 'on') {
  * Override or insert variables into all templates.
  */
 /* -- Delete this line if you want to use these functions
-function pixture_reloaded_preprocess(&$vars, $hook) {
+function adaptivetheme_subtheme_preprocess(&$vars, $hook) {
 }
-function pixture_reloaded_process(&$vars, $hook) {
+function adaptivetheme_subtheme_process(&$vars, $hook) {
 }
 // */
 
 /**
  * Override or insert variables into the html templates.
  */
-
-function pixture_reloaded_preprocess_html(&$vars) {
+/* -- Delete this line if you want to use these functions
+function adaptivetheme_subtheme_preprocess_html(&$vars) {
   // Uncomment the folowing line to add a conditional stylesheet for IE 7 or less.
   // drupal_add_css(path_to_theme() . '/css/ie/ie-lte-7.css', array('weight' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
-
-
 }
-function pixture_reloaded_process_html(&$vars) {
+function adaptivetheme_subtheme_process_html(&$vars) {
 }
 // */
 
@@ -65,9 +59,9 @@ function pixture_reloaded_process_html(&$vars) {
  * Override or insert variables into the page templates.
  */
 /* -- Delete this line if you want to use these functions
-function pixture_reloaded_preprocess_page(&$vars) {
+function adaptivetheme_subtheme_preprocess_page(&$vars) {
 }
-function pixture_reloaded_process_page(&$vars) {
+function adaptivetheme_subtheme_process_page(&$vars) {
 }
 // */
 
@@ -75,9 +69,9 @@ function pixture_reloaded_process_page(&$vars) {
  * Override or insert variables into the node templates.
  */
 /* -- Delete this line if you want to use these functions
-function pixture_reloaded_preprocess_node(&$vars) {
+function adaptivetheme_subtheme_preprocess_node(&$vars) {
 }
-function pixture_reloaded_process_node(&$vars) {
+function adaptivetheme_subtheme_process_node(&$vars) {
 }
 // */
 
@@ -85,9 +79,9 @@ function pixture_reloaded_process_node(&$vars) {
  * Override or insert variables into the comment templates.
  */
 /* -- Delete this line if you want to use these functions
-function pixture_reloaded_preprocess_comment(&$vars) {
+function adaptivetheme_subtheme_preprocess_comment(&$vars) {
 }
-function pixture_reloaded_process_comment(&$vars) {
+function adaptivetheme_subtheme_process_comment(&$vars) {
 }
 // */
 
@@ -95,8 +89,8 @@ function pixture_reloaded_process_comment(&$vars) {
  * Override or insert variables into the block templates.
  */
 /* -- Delete this line if you want to use these functions
-function pixture_reloaded_preprocess_block(&$vars) {
+function adaptivetheme_subtheme_preprocess_block(&$vars) {
 }
-function pixture_reloaded_process_block(&$vars) {
+function adaptivetheme_subtheme_process_block(&$vars) {
 }
 // */
