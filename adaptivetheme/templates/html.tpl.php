ffde7ef53517e03c91641689742841696c357f41
Jeff Burnz <git@adaptivethemes.com>
2011-02-05 20:02:38 +0000

    fix E_STRICT issue with RDF variables

--- adaptivetheme/templates/html.tpl.php
+++ adaptivetheme/templates/html.tpl.php
@@ -2,8 +2,8 @@
 // $Id$
 ?>
 <?php print $doctype; ?>
-<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
-<head<?php print $rdf->profile; ?>>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_version . $rdf_namespaces; ?>>
+<head<?php print $rdf_profile; ?>>
 <?php print $head; ?>
 <title><?php print $head_title; ?></title>
 <?php print $styles; ?>
