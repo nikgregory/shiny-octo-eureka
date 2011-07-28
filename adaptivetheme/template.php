cf0b03ffea9ab1623a9f6a96925cc888f1b22b29
Jeff Burnz <jeff@adaptivethemes.com>
2011-07-28 15:18:56 +0200

    remove old js file

--- adaptivetheme/template.php
+++ adaptivetheme/template.php
@@ -1,6 +1,4 @@
 <?php
-
 include_once(drupal_get_path('theme', 'adaptivetheme') . '/inc/template.theme.inc');
-include_once(drupal_get_path('theme', 'adaptivetheme') . '/inc/template.js.inc');
 include_once(drupal_get_path('theme', 'adaptivetheme') . '/inc/template.process.inc');
 include_once(drupal_get_path('theme', 'adaptivetheme') . '/inc/template.alter.inc');
