ccecbe9bc57eac9d074ebeec516fa1dac2677251
U-Jeff-VAIO\Jeff <Jeff@Jeff-VAIO.(none)>
2011-09-09 12:19:41 +0200

    remove custom styles for forum pngs, add theme button override for the admin theme

--- adaptivetheme_admin/template.php
+++ adaptivetheme_admin/template.php
@@ -9,3 +9,20 @@ function adaptivetheme_admin_preprocess_page(&$vars) {
   $vars['datetime_rfc'] = date("r" , time()); // RFC2822 date format
   $vars['datetime_iso'] = date("c" , time()); // ISO 8601 date format
 }
+
+/**
+* Reset theme button styles for admin to prevent issues with
+* Views and other modules
+*/
+function adaptivetheme_admin_button($vars) {
+  $element = $vars['element'];
+  $element['#attributes']['type'] = 'submit';
+  element_set_attributes($element, array('id', 'name', 'value'));
+
+  $element['#attributes']['class'][] = 'form-' . $element['#button_type'];
+  if (!empty($element['#attributes']['disabled'])) {
+    $element['#attributes']['class'][] = 'form-button-disabled';
+  }
+
+  return '<input' . drupal_attributes($element['#attributes']) . ' />';
+}
