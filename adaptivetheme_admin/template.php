6f6bd5c153a18168411f854d202711d9378b94ea
U-Jeff-VAIO\Jeff <Jeff@Jeff-VAIO.(none)>
2011-09-14 10:49:42 +0200

    big style update to admin theme, update google web fonts, add styles to gpanels for responsiveness, add figure and figcaption reset to base css

--- adaptivetheme_admin/template.php
+++ adaptivetheme_admin/template.php
@@ -9,18 +9,17 @@ function adaptivetheme_admin_preprocess_page(&$vars) {
   $vars['datetime_rfc'] = date("r" , time()); // RFC2822 date format
   $vars['datetime_iso'] = gmdate('Y-m-d\TH:i:s\Z'); // ISO 8601 date format
 }
+
 /**
-* Theme button.
+* Theme button. Override AT Core because it screws with Views.
 */
 function adaptivetheme_admin_button($vars) {
   $element = $vars['element'];
   $element['#attributes']['type'] = 'submit';
   element_set_attributes($element, array('id', 'name', 'value'));
-
   $element['#attributes']['class'][] = 'form-' . $element['#button_type'];
   if (!empty($element['#attributes']['disabled'])) {
     $element['#attributes']['class'][] = 'form-button-disabled';
   }
-
   return '<input' . drupal_attributes($element['#attributes']) . ' />';
 }
--- adaptivetheme_admin/templates/page.tpl.php
+++ adaptivetheme_admin/templates/page.tpl.php
@@ -1,38 +1,44 @@
 <?php // at-admin ?>
-<div id="page" class="container">
-  <header id="banner" class="clearfix">
-    <section id="breadcrumb"><?php print $breadcrumb; ?></section>
-    <?php if ($datetime_rfc): ?>
-      <time datetime="<?php print $datetime_iso; ?>"><?php print $datetime_rfc; ?></time>
-    <?php endif; ?>
-  </header>
-  <div id="main-content-header">
-    <?php print $breadcrumb; ?>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?><h1 id="page-title"><?php print $title; ?></h1><?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php if ($primary_local_tasks): ?>
-      <div id="primary-tasks"><ul class="tabs primary"><?php print render($primary_local_tasks); ?></ul></div>
-    <?php endif; ?>
-  </div>
-  <div id="columns"><div class="columns-inner clearfix">
-    <div id="content-column"><div class="content-inner">
-      <?php if ($secondary_local_tasks): ?>
-        <div id="secondary-tasks"><ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul></div>
+<div id="page">
+  <div class="header"><div class="container">
+    <header id="banner" class="clearfix">
+      <section id="breadcrumb"><?php print $breadcrumb; ?></section>
+      <?php if ($datetime_rfc): ?>
+        <time datetime="<?php print $datetime_iso; ?>"><?php print $datetime_rfc; ?></time>
       <?php endif; ?>
-      <?php print $messages; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-      <div id="main-content">
-        <?php print render($page['content']); ?>
-      </div>
-    </div></div>
-    <?php print render($page['sidebar_first']); ?>
-    <?php print render($page['sidebar_second']); ?>
+    </header>
   </div></div>
-  <?php if ($page['footer']): ?>
-    <footer id="page-footer">
-      <?php print render($page['footer']); ?>
-    </footer>
-  <?php endif; ?>
+  <div class="content-header"><div class="container clearfix">
+    <div id="main-content-header">
+      <?php print $breadcrumb; ?>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?><h1 id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php if ($primary_local_tasks): ?>
+        <nav id="primary-tasks" class="clearfix"><ul class="tabs primary"><?php print render($primary_local_tasks); ?></ul></nav>
+      <?php endif; ?>
+    </div>
+  </div></div>
+  <div class="content-columns"><div class="container clearfix">
+    <div id="columns"><div class="columns-inner clearfix">
+      <div id="content-column"><div class="content-inner">
+        <?php if ($secondary_local_tasks): ?>
+          <nav id="secondary-tasks" class="clearfix"><ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?></ul></nav>
+        <?php endif; ?>
+        <?php print $messages; ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <div id="main-content">
+          <?php print render($page['content']); ?>
+        </div>
+      </div></div>
+      <?php print render($page['sidebar_first']); ?>
+      <?php print render($page['sidebar_second']); ?>
+    </div></div>
+    <?php if ($page['footer']): ?>
+      <footer id="page-footer">
+        <?php print render($page['footer']); ?>
+      </footer>
+    <?php endif; ?>
+  </div></div>
 </div>
