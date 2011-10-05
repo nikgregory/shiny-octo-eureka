f3977534430248b61263b512c5f51095f677a75a
Jeff Burnz <jeff@adaptivethemes.com>
2011-10-05 13:12:30 +0200

    remove doctype swtich, add support gmap views

--- adaptivetheme/templates/html.tpl.php
+++ adaptivetheme/templates/html.tpl.php
@@ -1,10 +1,10 @@
-<?php print $doctype; ?>
+<!DOCTYPE html>
 <!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (lte IE 6)&(!IEMobile)]><html class="ie6 ie6-7 ie6-8" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (IE 7)&(!IEMobile)]><html class="ie7 ie6-7 ie6-8" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (IE 8)&(!IEMobile)]><html class="ie8 ie6-8" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
-<head<?php print $rdf_profile; ?>>
+<head>
 <?php print $head; ?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="MobileOptimized" content="width">
