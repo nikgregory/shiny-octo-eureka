499cb3ec7a01aee1fde1ac8571472333e9f7d6e9
Jeff Burnz <jeff@adaptivethemes.com>
2012-03-07 05:45:12 +0100

    add back the block content wrapper and attributes, update docs and functions to deal with the block content wrapper

--- adaptivetheme/templates/block.tpl.php
+++ adaptivetheme/templates/block.tpl.php
@@ -5,22 +5,24 @@
  *
  * The block template in Adaptivetheme is a little different to most other themes.
  * Instead of hard coding its markup Adaptivetheme generates most of it in
- * adaptivetheme_process_block(), conditionally printing outer and inner wrappers. 
- * 
+ * adaptivetheme_process_block(), conditionally printing outer and inner wrappers.
+ *
  * This allows the core theme to have just one template instead of five.
  *
- * You can override this in your sub-theme with a normal block suggestion and use 
+ * You can override this in your sub-theme with a normal block suggestion and use
  * a standard block template if you prefer, or use your own themeName_process_block()
  * function to control the markup.
  *
  * Adativetheme supplied variables:
- * - $outer_prefix: Holds a conditional element such as nav, section or div and 
+ * - $outer_prefix: Holds a conditional element such as nav, section or div and
  *                  includes the block id, classes and attributes.
  * - $outer_suffix: Closing element.
  * - $inner_prefix: Inner div with .block-inner and .clearfix classes.
  * - $inner_suffix: Closing div.
  * - $title: Holds the block->subject.
-
+ * - $content_processed: Pre-wrapped in div and attributes, but for some
+ *   blocks these are stripped away, e.g. menu bar and main content.
+ *
  * Available variables:
  * - $block->subject: Block title.
  * - $content: Block content.
@@ -67,5 +69,5 @@
   <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-  <?php print $content ?>
+  <?php print $content_processed; ?>
 <?php print $inner_suffix . $outer_suffix; ?>
