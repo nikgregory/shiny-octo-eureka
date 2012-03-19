dbb1c4b6c219ad858b95cbff1019d2729760ce15
Jeff Burnz <jeff@adaptivethemes.com>
2012-03-19 01:43:52 +0100

    update template.php comments

--- at_core/template.php
+++ at_core/template.php
@@ -3,15 +3,19 @@
  * IMPORTANT WARNING: DO NOT MODIFY THIS FILE OR ANY OF THE INCLUDED FILES.
  */
 
-// Set a global for path_to_at_core, we use this everywhere
-global $path_to_at_core;
-$path_to_at_core = DRUPAL_ROOT . '/' . drupal_get_path('theme', 'adaptivetheme');
+// Set globals for often used stuff.
+global $theme_name, $path_to_at_core;
 
-global $theme_name;
+// $theme_key is not always what we need, so for consistancy AT uses its own global $theme_name
+// and depending on the context populates it as needed. Know that when you see $theme_name it
+// always means the currently active theme.
 $theme_name = $GLOBALS['theme'];
 
-// Include the most essential files to make this mofo run
-include_once($path_to_at_core . '/inc/template.theme.inc');
-include_once($path_to_at_core . '/inc/template.alter.inc');
-include_once($path_to_at_core . '/inc/template.preprocess.inc');
-include_once($path_to_at_core . '/inc/template.process.inc');
+// Absolute path to AT core, used for include files only.
+$path_to_at_core = DRUPAL_ROOT . '/' . drupal_get_path('theme', 'adaptivetheme');
+
+// Include the most essential files to make this mofo run.
+include_once($path_to_at_core . '/inc/template.theme.inc');      // theme function overrides
+include_once($path_to_at_core . '/inc/template.alter.inc');      // hook_alters
+include_once($path_to_at_core . '/inc/template.preprocess.inc'); // all preprocess functions
+include_once($path_to_at_core . '/inc/template.process.inc');    // all process functions
