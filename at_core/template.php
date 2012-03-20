0ab11f39e056b042917492d2230f2cec7dc3c3a4
Jeff Burnz <jeff@adaptivethemes.com>
2012-03-20 11:50:05 +0100

    add check for 7.x-2.x subthemes and load googl web fonts so the dont break, minor CSS formatting updates and additional comments in many files, remove font size in debug-regions css, general cleanups as we move towards an alpha

--- at_core/template.php
+++ at_core/template.php
@@ -6,10 +6,9 @@
 // Set globals for often used stuff.
 global $theme_name, $path_to_at_core;
 
-// $theme_key is not always what we need, so for consistancy AT uses its own global $theme_name
-// and depending on the context populates it as needed. Know that when you see $theme_name it
-// always means the currently active theme.
-$theme_name = $GLOBALS['theme'];
+// $theme_key is not always what we need, so for consistancy AT uses its own 
+// global $theme_name and depending on the context populates it as needed.
+$theme_name = $GLOBALS['theme_key'];
 
 // Absolute path to AT core, used for include files only.
 $path_to_at_core = DRUPAL_ROOT . '/' . drupal_get_path('theme', 'adaptivetheme');
@@ -19,3 +18,14 @@ include_once($path_to_at_core . '/inc/template.theme.inc');      // theme functi
 include_once($path_to_at_core . '/inc/template.alter.inc');      // hook_alters
 include_once($path_to_at_core . '/inc/template.preprocess.inc'); // all preprocess functions
 include_once($path_to_at_core . '/inc/template.process.inc');    // all process functions
+
+// Hi-jinks to support 7.x-2.x sub-themes.
+$lt = list_themes();
+foreach ($lt as $key => $value) {
+  if ($theme_name == $key) {
+    $info = $lt[$theme_name]->info;
+    if (!array_key_exists('release', $info) || ($info['release'] == '7.x-2.x')) {
+      include_once($path_to_at_core . '/inc/google.web.fonts.inc');
+    }
+  }
+}
