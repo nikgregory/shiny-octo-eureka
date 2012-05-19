9b3c467800daab0d1479e96138a1126b01a9ac04
Jeff Burnz <jeff@adaptivethemes.com>
2012-05-19 18:35:57 +0200

    add attributes to more wrappers in node and comment templates, shuffle process functions, remove redundant hook param from all pre and process functions

--- at_core/templates/comment.tpl.php
+++ at_core/templates/comment.tpl.php
@@ -64,7 +64,7 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title || $new): ?>
-  <header>
+  <header<?php print $header_attributes; ?>>
     <h3<?php print $title_attributes; ?>><?php print $title ?></h3>
     <?php if ($new): ?>
       <em class="new"><?php print $new ?></em>
@@ -73,7 +73,7 @@
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
-  <footer class="submitted<?php $picture ? print ' with-picture' : ''; ?>">
+  <footer<?php print $footer_attributes; ?>>
     <?php print $picture; ?>
     <p class="author-datetime"><?php print $submitted; ?></p>
   </footer>
@@ -90,7 +90,7 @@
   <?php endif; ?>
 
   <?php if ($links = render($content['links'])): ?>
-    <nav class="clearfix"><?php print $links; ?></nav>
+    <nav<?php print $links_attributes; ?>><?php print $links; ?></nav>
   <?php endif; ?>
 
 </article>
--- at_core/templates/node.tpl.php
+++ at_core/templates/node.tpl.php
@@ -95,7 +95,7 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title && !$page): ?>
-    <header>
+    <header<?php print $header_attributes; ?>>
       <?php if ($title): ?>
         <h1<?php print $title_attributes; ?>>
           <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a>
@@ -106,7 +106,7 @@
   <?php print render($title_suffix); ?>
 
   <?php if ($display_submitted): ?>
-    <footer class="submitted<?php $user_picture ? print ' with-user-picture' : ''; ?>">
+    <footer<?php print $footer_attributes; ?>>
       <?php print $user_picture; ?>
       <p class="author-datetime"><?php print $submitted; ?></p>
     </footer>
@@ -121,7 +121,7 @@
   </div>
 
   <?php if ($links = render($content['links'])): ?>
-    <nav class="clearfix"><?php print $links; ?></nav>
+    <nav<?php print $links_attributes; ?>><?php print $links; ?></nav>
   <?php endif; ?>
 
   <?php print render($content['comments']); ?>
