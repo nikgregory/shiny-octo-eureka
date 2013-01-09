9a412987c3208600d5bedfa5ecaf4f1a11577c28
Jeff Burnz <jeff@adaptivethemes.com>
2013-01-09 14:25:54 +0100

    remove white space from page template, improve help messages in some forms, fix issue with load inc where the wrong media could be used in certain circumstances, fix wrong selector in menu toggle js

--- at_core/scripts/menu-toggle.js
+++ at_core/scripts/menu-toggle.js
@@ -2,9 +2,9 @@
   Drupal.behaviors.ATmenuToggle = {
     attach: function (context, settings) {
       $("#primary-menu-bar, #menu-bar").addClass('at-menu-toggle');
-      $(".at-menu-toggle h2").removeClass('element-invisible').wrapInner('<a href="#menu-toggle" class="at-menu-toggle-link" />');
+      $(".at-menu-toggle h2").removeClass('element-invisible').addClass('at-menu-toggle-button').wrapInner('<a href="#menu-toggle" class="at-menu-toggle-button-link" />');
       $(".at-menu-toggle nav > ul[class*=menu]:nth-of-type(1)").wrap('<div id="menu-toggle" />');
-      $(".at-menu-toggle h2 .at-menu-toggle-link").click(function() {
+      $(".at-menu-toggle h2 .at-menu-toggle-button-link").click(function() {
         $(".at-menu-toggle #menu-toggle").slideToggle('fast');
         return false;
       });
