ee4c7724dd9595b9e117e8f36f1f87d818c25023
jmburnz <jeff@adaptivethemes.com>
2013-03-06 22:36:30 +0100

    Add new features for menu toggles -  allow users to chose which menus and regions

--- at_core/scripts/menu-toggle.js
+++ at_core/scripts/menu-toggle.js
@@ -15,9 +15,8 @@
       if (mtsTP == 'true') { breakpoints.push(bp3 + ':' + themeSettings[activeTheme]['media_query_settings']['tablet_portrait']); }
       if (mtsTL == 'true') { breakpoints.push(bp4 + ':' + themeSettings[activeTheme]['media_query_settings']['tablet_portrait']); }
 
-      $("#secondary-menu-bar, #primary-menu-bar, #menu-bar").addClass('at-menu-toggle');
       $(".at-menu-toggle h2").removeClass('element-invisible').addClass('at-menu-toggle-button').wrapInner('<a href="#menu-toggle" class="at-menu-toggle-button-link" />');
-      $(".at-menu-toggle nav > ul[class*=menu]:nth-of-type(1)").wrap('<div id="menu-toggle" />');
+      $(".at-menu-toggle ul[class*=menu]:nth-of-type(1)").wrap('<div class="menu-toggle" />');
 
       !function(breakName, query){
 
