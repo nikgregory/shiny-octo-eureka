c4b47ca336677206f0d648d98cb36d92cf6563b1
jmburnz <jeff@adaptivethemes.com>
2014-12-05 22:36:30 +0100

    Phase 1 layouts optimisations, grouping selectors, add sass-globbing

--- at_generator/components/starterkit/layout/site-builder/config.rb
+++ at_generator/components/starterkit/layout/site-builder/config.rb
@@ -3,6 +3,7 @@
 
 # Required
 # -----------------------------------------------------------------------------
+require 'sass-globbing'
 require "susy"
 
 
@@ -19,22 +20,9 @@ Sass::Script::Number.precision = 5
 
 # Environment
 # -----------------------------------------------------------------------------
-#environment = :development
 environment = :production
 
 
 # Output Style
 # -----------------------------------------------------------------------------
-# Drupal will aggregate and compress stylesheets, we only need expanded, or
-# nested if that's how you roll.
-output_style = :expanded
-
-
-# Line Comments
-# -----------------------------------------------------------------------------
-#line_comments = (environment == :development) ? true : false
-
-
-# Sourcemaps
-# -----------------------------------------------------------------------------
-#sourcemap = (environment == :development) ? true : false
+output_style = :compact
