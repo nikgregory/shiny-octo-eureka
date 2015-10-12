3cf54e12a2de0c33896a845a363d7a41f935a8ad
jmburnz <jeff@adaptivethemes.com>
2015-10-12 16:21:09 +1300

    Clean up for use with Yaml::encode and remove redundant extra lines
    Signed-off-by: jmburnz <jeff@adaptivethemes.com>

--- at_generator/src/Theme/ThemeGeneratorSubmit.php
+++ at_generator/src/Theme/ThemeGeneratorSubmit.php
@@ -26,7 +26,6 @@ class ThemeGeneratorSubmit {
     $fileOperations      = new FileOperations();
     $directoryOperations = new DirectoryOperations();
 
-
     // Prepare form values and set them into variables
     $machine_name    = $values['generate']['generate_machine_name'];
     $friendly_name   = SafeMarkup::checkPlain($values['generate']['generate_friendly_name']);
@@ -42,15 +41,12 @@ class ThemeGeneratorSubmit {
     $description         = preg_replace('/[^A-Za-z0-9. ]/', '', $values['generate']['options']['generate_description']);
     $version             = $values['generate']['options']['generate_version'];
 
-
     // Generic description if the desc field is not set
     $generic_decription = 'Sub theme of AT Core';
-
 
     // Path to themes
     $path = drupal_get_path('theme', 'at_core');
     $at_generator_path = drupal_get_path('theme', 'at_generator');
-
 
     // Path to where we will save the cloned theme
     // This could be configurable?
@@ -65,7 +61,6 @@ class ThemeGeneratorSubmit {
     $target_dir = $directoryOperations->directoryPrepare($target_path);
     $target = "$target_dir/$machine_name";
 
-
     // Array of UIKit tools
     $uikit_tools = array(
       'package.json',
@@ -75,13 +70,11 @@ class ThemeGeneratorSubmit {
       'Gemfile.lock',
     );
 
-
     // Standard and Minimal variables
     if ($subtheme_type === 'standard') {
       $source_theme = 'THEMENAME';
       $source = $at_generator_path . '/components/starterkit';
     }
-
 
     // Clone variables
     if ($subtheme_type === 'clone') {
@@ -89,33 +82,27 @@ class ThemeGeneratorSubmit {
       $source = drupal_get_path('theme', $source_theme);
     }
 
-
     // Skin variables
     if ($subtheme_type === 'skin') {
       $source_theme = $skin_base_theme;
       $source = drupal_get_path('theme', $source_theme);
     }
 
-
     // All themes have configuration
     $configuration_files = $directoryOperations->directoryScan("$source/config/install");
-
 
     // Files to strip replace strings
     $info_file      = "$target/$machine_name.info.yml";
     $library_file   = "$target/$machine_name.libraries.yml";
     $shortcodes_file = "$target/$machine_name.shortcodes.yml";
 
-
     // Begin generation
     //------------------------------------------------------------------------------------------------
-
 
     // Recursively copy the source theme
     if (is_dir($source)) {
       $directoryOperations->directoryRecursiveCopy($source, $target);
     }
-
 
     // Genearated CSS files
     $generated_css = $directoryOperations->directoryScan("$source/styles/css/generated");
@@ -123,7 +110,6 @@ class ThemeGeneratorSubmit {
       $new_css_file = str_replace($source_theme, $machine_name, $old_css_file);
       $fileOperations->fileRename("$target/styles/css/generated/$old_css_file", "$target/styles/css/generated/$new_css_file");
     }
-
 
     // UIKit and Color
     if ($subtheme_type === 'standard') {
@@ -152,12 +138,10 @@ class ThemeGeneratorSubmit {
       }
     }
 
-
     // UIKit skins
     if ($subtheme_type === 'skin') {
       $directoryOperations->directoryRemove("$target/styles/uikit");
     }
-
 
     // Templates
     if ($subtheme_type === 'standard') {
@@ -168,7 +152,6 @@ class ThemeGeneratorSubmit {
     if ($subtheme_type === 'skin') {
       $directoryOperations->directoryRemove("$target/templates");
     }
-
 
     // .theme
     if ($subtheme_type === 'standard') {
@@ -185,7 +168,6 @@ class ThemeGeneratorSubmit {
       $fileOperations->fileReplace($skin_theme, "$target/$machine_name.theme");
       $fileOperations->fileStrReplace("$target/$machine_name.theme", 'HOOK', $machine_name);
     }
-
 
     // libraries
     $fileOperations->fileRename("$target/$source_theme.libraries.yml", $library_file);
@@ -207,9 +189,7 @@ class ThemeGeneratorSubmit {
       //$fileOperations->fileRename("$target/styles/css/styles.css", "$target/styles/css/$machine_name-styles.css");
       //$skin_styles = '/* Styles for ' . $machine_name . ' */';
       //$fileOperations->fileReplace($skin_styles, "$target/styles/css/$machine_name-styles.css");
-
     }
-
 
     // theme-settings.php
     if ($subtheme_type === 'standard') {
@@ -226,7 +206,6 @@ class ThemeGeneratorSubmit {
     if ($subtheme_type === 'skin') {
       $directoryOperations->directoryRemove("$target/theme-settings.php");
     }
-
 
     // Config
     foreach ($configuration_files as $old_config_file) {
@@ -250,30 +229,24 @@ class ThemeGeneratorSubmit {
       }
     }
 
-
     // Info
     $fileOperations->fileRename("$target/$source_theme.info.yml", $info_file);
 
-
     // Shortcodes
     $fileOperations->fileRename("$target/$source_theme.shortcodes.yml", $shortcodes_file);
-
 
     // Parse, rebuild and save the themes info.yml file.
     $parser = new Parser();
     $theme_info_data = $parser->parse(file_get_contents($info_file));
 
-
     // Name and theme type
-    $theme_info_data['name'] = "'$friendly_name'";
+    $theme_info_data['name'] = "$friendly_name";
     $theme_info_data['type'] = "theme";
-
 
     // Base theme
     if ($subtheme_type === 'skin') {
       $theme_info_data['base theme'] = $source_theme;
     }
-
 
     // Description
     $base_theme = $theme_info_data['base theme'];
@@ -285,71 +258,66 @@ class ThemeGeneratorSubmit {
       $description = $description ?: 'Skin theme';
     }
     $description = $description ?: $generic_decription;
-    $theme_info_data['description'] = "'$description (base theme: $base_theme)'";
-
+    $theme_info_data['description'] = "$description (base theme: $base_theme)";
 
     // alt text
-    $theme_info_data['alt text'] = "'Screenshot for $friendly_name'";
+    $theme_info_data['alt text'] = "Screenshot for $friendly_name";
 
     // Version
     $version = $version ?: '8.0.x';
     $theme_info_data['version']     = $version;
 
-
-    // Libraries
-    //$theme_info_data['libraries'] = array(
-    //  "$machine_name/base",
-    //);
-
-
     // Regions
     foreach($theme_info_data['regions'] as $region_key => $region_name) {
-      $theme_info_data['regions'][$region_key] = "'$region_name'";
+      $theme_info_data['regions'][$region_key] = "$region_name";
     }
-
 
     // Unset hidden
     unset($theme_info_data['hidden']);
-
 
     // Unset stylesheets-remove
     //if ($subtheme_type === 'skin') {
     //  unset($theme_info_data['stylesheets-remove']);
     //}
 
+    // Libraries
+    $theme_info_data['libraries-override'] = $theme_info_data['libraries-override'];
+
     // Save the info file
     $rebuilt_info = $fileOperations->fileBuildInfoYml($theme_info_data);
     $fileOperations->fileReplace($rebuilt_info, $info_file);
-
-
-    // Reset the themes list, see if we can get this to show up without having the clear the cache manually.
-    system_list_reset();
-    \Drupal::service('theme_handler')->rebuildThemeData();
 
 
     // Set messages, however we may need more validation?
     //----------------------------------------------------------------------
     $generated_path = drupal_get_path('theme', $machine_name);
 
+    // system message for Reports.
+    $logger_message = t('A new theme <b>@theme_name</b>, with then machine name: <code><b>@machine_name</b></code>, has been generated.', array('@theme_name' => $friendly_name, '@machine_name' => $machine_name));
+    \Drupal::logger('at_generator')->notice($logger_message);
 
+    // message for the user
     drupal_set_message(t("<p>A new theme <b>@theme_name</b>, with then machine name: <code><b>@machine_name</b></code>, has been generated.</p><p>You can find your theme here: <code><b>@theme_path</b></code> </p>", array('@theme_name' => $friendly_name, '@machine_name' => $machine_name, '@theme_path' => $target, '@performance_settings' => base_path() . 'admin/config/development/performance')), 'status');
 
 
-/*
+    // Refresh data.
+    system_list_reset();
+    \Drupal::service('theme_handler')->rebuildThemeData();
+
+
+    /*
     // Warn about stylesheets in the new skin theme
     if ($subtheme_type === 'at_skin') {
       drupal_set_message(t('Skin themes do not inherit theme <em>settings</em>, this is important for things like Layout and Library settings. After you enable your new theme be sure to check and configure it\'s settings.', array('!theme_path' => $generated_path, '!machine_name' => $machine_name)), 'warning');
     }
-*/
+   */
   }
 
-/*
+  /*
   else {
     // TODO check if this is validated, really this should be in validation.
     $source = $path . '/../at_starterkits/' . $subtheme_type;
     drupal_set_message(t("An error occurred and processing did not complete. The source directory '!dir' does not exist or is not readable.", array('!dir' => $source)), 'error');
   }
-*/
+  */
 }
-
-
