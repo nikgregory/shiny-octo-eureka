d82ae0f1a35314eb25c356d473d0cecd19ad7d72
jmburnz <jeff@adaptivethemes.com>
2015-11-19 22:02:20 +1300

    Finalize Responsive Table JS
    Signed-off-by: jmburnz <jeff@adaptivethemes.com>

--- at_core/scripts/at.responsiveTables.js
+++ at_core/scripts/at.responsiveTables.js
@@ -8,11 +8,11 @@
   "use strict";
 
   $('table.is-responsive').wrap('<div class="responsive-table"><div class="responsive-table__scroll"></div></div>');
-  //$('.responsive-table').prepend('<div class="responsive-table__fade"></div>');
 
-  if ($("table.is-responsive").prop('scrollWidth') > $(".responsive-table").width() ) {
+  if ($('table.is-responsive').prop('scrollWidth') > $(".responsive-table").width() ) {
     var overflowmessage = Drupal.t('Scroll to view');
     $('.responsive-table__scroll').append('<div class="responsive-table__message"><em>' + overflowmessage + '</em></div>');
+    $('table.is-responsive').addClass('has-overflow');
   }
 
 })(jQuery, Drupal);
