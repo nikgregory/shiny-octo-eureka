e75420e37e960e13434b8756665e54b5d641e2c4
jmburnz <jeff@adaptivethemes.com>
2015-12-16 20:13:37 +1300

    Updates for Responsive menu block, JS and CSS
    Signed-off-by: jmburnz <jeff@adaptivethemes.com>

--- at_core/scripts/at.responsiveMenus.js
+++ at_core/scripts/at.responsiveMenus.js
@@ -15,26 +15,32 @@
       var at   = settings['ajaxPageState']['theme'],
           rm   = settings[at]['at_responsivemenus'],
           def  = rm['default'],
-          resp = rm['responsive'];
+          resp = rm['responsive'],
+          tl   = '.rm-block .rm-toggle__link',
+          exp  = $(tl).attr("aria-expanded") == "false";
 
       // Toggle handler.
       function toggleClick(e) {
         e.preventDefault();
         e.stopPropagation();
 
-        // The toggle class is on <body> because we must account
-        // for menu types that style block parent elements,
-        // e.g. off-canvas will transform the .page element.
+        // The toggle class is on <body> because we must account for menu types
+        // that style block parent elements, e.g. off-canvas will transform the
+        // .page element. Toggle aria attributes for accessibility on block
+        // elements.
         $(document.body).toggleClass('rm-is-open');
+        $(tl).attr("aria-expanded", exp ? "true" : "false");
+
         $(document).one('click', function(e) {
           if($('.rm-block').has(e.target).length === 0){
             $(document.body).removeClass('rm-is-open');
+            $(tl).attr("aria-expanded", "false");
           }
         });
       }
 
       // Toggle.
-      $('.rm-toggle', context).on('click', toggleClick);
+      $(tl, context).on('click', toggleClick);
 
       // Enquire is a fancy wrapper for matchMedia.
       enquire
@@ -45,8 +51,8 @@
           $('.rm-block').parent('.l-r').addClass('rm-region');
           $('.rm-block').parent().parent('.l-rw').addClass('rm-row');
         },
-        // The resp menu system only uses one breakpoint,
-        // if it matches this fires strait after setup.
+        // The resp menu system only uses one breakpoint, if it matches this
+        // fires strait after setup.
         match: function() {
           if(resp !== 'ms-none') {
             if(resp !== def) {
