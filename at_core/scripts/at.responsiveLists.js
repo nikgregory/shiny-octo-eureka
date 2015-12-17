7887ec7698fa3eac24a09de62f6282937455782a
jmburnz <jeff@adaptivethemes.com>
2015-12-17 21:07:43 +1300

    Fix issues with responsive lists js and breadcrumbs
    Signed-off-by: jmburnz <jeff@adaptivethemes.com>

--- at_core/scripts/at.responsiveLists.js
+++ at_core/scripts/at.responsiveLists.js
@@ -1,10 +1,12 @@
 /**
  * @file
- * Responsive navigation list_items. This is pretty much a ripoff
- * of the Seven themes responsive tabs JS, except I have generalized
- * it so we can use it for any list. In AT we use this for both
- * tabs (local tasks) and breadcrumbs. See the breadcrumbs template
- * and SCSS partial to see how this works.
+ * Responsive Lists
+ * This is based on the the Seven themes responsive tabs JS, except I have
+ * generalized it so we can use it for any list. Also we use outerWidth and
+ * calculate the total width of all list items.
+ *
+ * In AT we use this for both tabs (local tasks) and breadcrumbs. See the
+ * breadcrumbs template and SCSS partial to see how this works.
  */
 (function ($, Drupal) {
 
@@ -12,27 +14,36 @@
 
   function init(i, list_item) {
 
-    var $list_item = $(list_item);
+    var list_item = $(list_item);
 
     function handleResize(e) {
-      $list_item.addClass('is-horizontal');
-      var $list_items = $list_item.find('.is-responsive__list');
-      var isHorizontal = $list_items.outerHeight() <= $list_items.find('.is-responsive__item').outerHeight();
-      if (isHorizontal) {
-        $list_item.removeClass('is-vertical');
+      list_item.addClass('is-horizontal');
+
+      var lists = list_item.find('.is-responsive__list');
+      var list_items_width = 0;
+
+      lists.find('.is-responsive__item').each(function() {
+        list_items_width += $(this).outerWidth(true);
+      });
+
+      var isHorizontal = lists.outerWidth(true) <= list_items_width;
+
+      if (isHorizontal ==  true) {
+        list_item.removeClass('is-horizontal').addClass('is-vertical');
       } else {
-        $list_item.removeClass('is-horizontal').addClass('is-vertical');
+        list_item.removeClass('is-vertical').addClass('is-horizontal');
       }
     }
 
-    $(window).on('resize.list_item', Drupal.debounce(handleResize, 150)).trigger('resize.list_item');
+    $(window).on('resize.lists', Drupal.debounce(handleResize, 150)).trigger('resize.lists');
   }
 
+  // Initialize the Responsive lists JS.
   Drupal.behaviors.atRL = {
-    attach: function (context, settings) {
-      var $list_items = $(context).find('[data-at-responsive-list]');
-      if ($list_items.length) {
-        $list_items.once().each(init);
+    attach: function (context) {
+      var list_items = $(context).find('[data-at-responsive-list]');
+      if (list_items.length) {
+        list_items.once().each(init);
       }
     }
   };
