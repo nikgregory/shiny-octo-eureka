1b4298866879c5bff29903a91da9af16c8b4919f
jmburnz <jeff@adaptivethemes.com>
2016-08-05 22:59:58 +1200

    add live reload option, remove old untested and out of date devel options

--- at_core/forms/ext/devel.php
+++ at_core/forms/ext/devel.php
@@ -5,6 +5,8 @@
  * Generate form elements for the Devel settings.
  */
 
+use Drupal\Component\Utility\Html;
+
 // Development settings
 $form['devel'] = array(
   '#type' => 'details',
@@ -29,13 +31,38 @@ $form['devel']['settings_show_window_size'] = array(
   '#default_value' => theme_get_setting('settings.show_window_size', $theme),
 );
 
-// Show grid
-$form['devel']['settings_show_grid'] = array(
-  '#type' => 'checkbox',
-  '#title' => t('Show the grid'),
-  '#default_value' => theme_get_setting('settings.show_grid', $theme),
-  '#description' => t('Show the grid. Your theme must include a CSS file named "show-grid.css" in the /css folder which should load the grid CSS or image file. All UIKit based themes have this file and will show a toggle icon in the bottom left corner of the screen - hover the icon to show the grid as an overlay. The CSS grid overlay is an approximation. Browsers have extra trouble with sub-pixel rounding on background images. <b>This is meant for rough debugging, not for pixel-perfect measurements</b>.'),
+// Enable LiveReload
+$form['devel']['live_reload'] = array(
+  '#type' => 'fieldset',
+  '#title' => t('Live Reload'),
 );
+
+$form['devel']['live_reload']['settings_enable_live_reload'] = array(
+  '#type' => 'checkbox',
+  '#title' => t('LiveReload'),
+  '#description' => t('Add the LiveReload script the page (this will trigger your LiveReload browser extension to connect).'),
+  '#default_value' => theme_get_setting('settings.enable_live_reload', $theme),
+);
+
+$form['devel']['live_reload']['settings_live_reload_port'] = array(
+  '#type' => 'textfield',
+  '#size' => 5,
+  '#maxlength' => 5,
+  '#title' => t('LiveReload Port'),
+  '#description' => t('Change the port number, this should match the port in your watch tasks in Gruntfile.js. By default this is set to 1337'),
+  '#default_value' => Html::escape(theme_get_setting('settings.live_reload_port', $theme)),
+  '#states' => array(
+    'visible' => array('input[name="settings_enable_live_reload"]' => array('checked' => TRUE)),
+  ),
+);
+
+// Show grid
+//$form['devel']['settings_show_grid'] = array(
+//  '#type' => 'checkbox',
+//  '#title' => t('Show the grid'),
+//  '#default_value' => theme_get_setting('settings.show_grid', $theme),
+//  '#description' => t('Show the grid. Your theme must include a CSS file named "show-grid.css" in the /css folder which should load the grid CSS or image file. All UIKit based themes have this file and will show a toggle icon in the bottom left corner of the screen - hover the icon to show the grid as an overlay. The CSS grid overlay is an approximation. Browsers have extra trouble with sub-pixel rounding on background images. <b>This is meant for rough debugging, not for pixel-perfect measurements</b>.'),
+//);
 
 // Debug layout
 //$form['devel']['settings_devel_layout'] = array(
@@ -46,15 +73,15 @@ $form['devel']['settings_show_grid'] = array(
 //);
 
 // Colorize regions.
-$form['devel']['settings_devel_color_regions'] = array(
-  '#type' => 'checkbox',
-  '#title' => t('Colorize regions'),
-  '#default_value' => theme_get_setting('settings.devel_color_regions', $theme),
-  '#description' => t('Add background color to regions. Also adds a margin-bottom for visual separation.'),
-  '#states' => array(
-    //'disabled' => array('input[name="settings_devel_layout"]' => array('checked' => TRUE)),
-  ),
-);
+//$form['devel']['settings_devel_color_regions'] = array(
+//  '#type' => 'checkbox',
+//  '#title' => t('Colorize regions'),
+//  '#default_value' => theme_get_setting('settings.devel_color_regions', $theme),
+//  '#description' => t('Add background color to regions. Also adds a margin-bottom for visual separation.'),
+//  '#states' => array(
+//    //'disabled' => array('input[name="settings_devel_layout"]' => array('checked' => TRUE)),
+//  ),
+//);
 
 // Neutralize Toolbar.
 $form['devel']['settings_nuke_toolbar'] = array(
